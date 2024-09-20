42ee83fdf93dbb8aaa48b51c76fed9e4b63c14bc
hashtag-sau <saurabhdubey0211@gmail.com>
2024-09-20 22:45:46 +0530

    got alarmed

---
+++ 1c.c
@@ -0,0 +1,47 @@
+/*
+============================================================================
+Name : 1a.c
+Author : Saurabh Dubey
+Description :1. Write a separate program (for each time domain) to set a interval timer in 10sec and
+10micro second
+a. ITIMER_REAL
+b. ITIMER_VIRTUAL
+c. ITIMER_PROF
+Date: 20th sep, 2024.
+============================================================================
+*/
+
+#include<stdio.h>
+#include<sys/time.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<signal.h>
+
+void handle_sigalarm(int sig){
+    printf("ALARM!! ITIMER_PROF timer expired\n");
+}
+
+int main(){
+
+    signal(SIGPROF,handle_sigalarm);
+    struct itimerval t;
+    t.it_value.tv_sec = 10;
+    t.it_value.tv_usec = 0;
+    t.it_interval.tv_sec = 0; 
+    t.it_interval.tv_usec = 10; 
+
+    setitimer(ITIMER_PROF, &t, NULL);
+    while(1); 
+}
+
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+// ALARM!! ITIMER_PROF timer expired
+
